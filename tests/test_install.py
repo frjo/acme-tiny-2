8d7d699858cc53676558c55c1f1fcd8ba24a530d
Fredrik Jonsson <frjo@xdeb.org>
2025-01-04 15:50:38 +0100

    Update github actions, part 8.

--- tests/test_install.py
+++ tests/test_install.py
@@ -4,4 +4,4 @@ import unittest
 
 class TestInstall(unittest.TestCase):
     def test_cli(self):
-        subprocess.check_call(["uv", "run", "acme-tiny-2", "-h"])
+        subprocess.check_call(["uv", "run", "acme-tiny-2.py", "-h"])
